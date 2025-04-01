0902634e3283dfe822c11de7e8311ffacb9b928a
洪华俊 <erfu81856@gmail.com>
2025-04-01 22:05:42 +0800

    update

---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/log-c847ac8c7d6a312b.d
@@ -0,0 +1,10 @@
+/home/hhj/2025s-rcore-hashcowuwu/os/target/riscv64gc-unknown-none-elf/release/deps/liblog-c847ac8c7d6a312b.rmeta: /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs
+
+/home/hhj/2025s-rcore-hashcowuwu/os/target/riscv64gc-unknown-none-elf/release/deps/liblog-c847ac8c7d6a312b.rlib: /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs
+
+/home/hhj/2025s-rcore-hashcowuwu/os/target/riscv64gc-unknown-none-elf/release/deps/log-c847ac8c7d6a312b.d: /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs /home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs
+
+/home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs:
+/home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs:
+/home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs:
+/home/hhj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-6ceace7a398a7c04.d
@@ -0,0 +1,13 @@
+/home/hhj/2025s-rcore-hashcowuwu/os/target/riscv64gc-unknown-none-elf/release/deps/os-6ceace7a398a7c04: src/main.rs src/console.rs src/lang_items.rs src/logging.rs src/sbi.rs src/boards/qemu.rs src/entry.asm
+
+/home/hhj/2025s-rcore-hashcowuwu/os/target/riscv64gc-unknown-none-elf/release/deps/os-6ceace7a398a7c04.d: src/main.rs src/console.rs src/lang_items.rs src/logging.rs src/sbi.rs src/boards/qemu.rs src/entry.asm
+
+src/main.rs:
+src/console.rs:
+src/lang_items.rs:
+src/logging.rs:
+src/sbi.rs:
+src/boards/qemu.rs:
+src/entry.asm:
+
+# env-dep:LOG
